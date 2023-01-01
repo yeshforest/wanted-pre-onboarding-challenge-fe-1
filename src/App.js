3a67879f0451648bf3605cf4ac3627558a3c2d5e
haerimRoh <mtxpvksh@gmail.com>
2023-01-02 00:25:01 +0900

    [feat] basic email input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./App.css";
+
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">hello world</header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/auth/login" element={<Login />}></Route>
+          <Route path="/auth/signup" element={<SignUp />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/SignUp.js
@@ -0,0 +1,10 @@
+const SignUp = () => {
+  return (
+    <div>
+      <h1>SignUp</h1>
+      <p>이곳은 회원가입 입니다.</p>
+    </div>
+  );
+};
+
+export default SignUp;
